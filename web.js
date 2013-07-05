195b15c88da0c8aa29552cbb72de6ad36d2730ce
Bryan Vu <bryanvu@gmail.com>
2013-07-05 00:14:49 -0700

    Serving from index.html

--- web.js
+++ web.js
@@ -1,9 +1,19 @@
+var fs = require('fs');
+
 var express = require('express');
 
 var app = express.createServer(express.logger());
 
+var str = '';
+
+fs.readFile('index.html', function (err, data) {
+  if (err) throw err;
+  str = new Buffer(data).toString();
+});
+
+
 app.get('/', function(request, response) {
-  response.send('Hello World!');
+  response.send(str);
 });
 
 var port = process.env.PORT || 5000;
